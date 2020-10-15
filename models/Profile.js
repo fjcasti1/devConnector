f78f81a4ffdfba99fb6fb853c5ea3f67044a4194
Francisco Castillo <fjcasti1@asu.edu>
2020-10-15 13:20:03 -0700

    Get current user profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.type.ObjectId,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
   company: { type: String },
