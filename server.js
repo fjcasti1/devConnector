aa2e679a41663264ed67932513a09feb5777b699
Francisco Castillo <fjcasti1@asu.edu>
2020-10-14 23:15:28 -0700

    Connect to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+// Initialize express server
 const app = express();
+
+// Connect to DB
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
