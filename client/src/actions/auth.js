24d3adeca74ff513f0d5fc51107f387a8488ba1e
Francisco Castillo <fjcasti1@asu.edu>
2020-10-18 17:50:14 -0700

    Load User & Set Auth Token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,7 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
 import axios from 'axios';
 import { setAlert } from './alert';
+import setAuthToken from '../utils/setAuthToken';
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
@@ -28,6 +29,24 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
     });
   }
 };
