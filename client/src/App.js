fc64f5694961b23aff35f8222bdd888490041eec
Francisco Castillo <fjcasti1@asu.edu>
2020-10-19 11:59:22 -0700

    Protected Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import Alert from './components/layout/Alert';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+// import setAuthToken from './utils/setAuthToken';
 
 // if (localStorage.token) {
 //   setAuthToken(localStorage.token);
@@ -30,6 +32,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Dashboard = () => {
+  return (
+    <Fragment>
+      <h1 class='large text-primary'>Dashboard</h1>
+    </Fragment>
+  );
+};
+
+export default Dashboard;
