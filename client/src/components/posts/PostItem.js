e13b432755334920651667aaec6d11b85192ad15
Francisco Castillo <fjcasti1@asu.edu>
2020-10-21 12:29:51 -0700

    Like & Unlike Functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { addLike, removeLike } from '../../actions/post';
 
 const PostItem = ({
   userID, //Logged in user
   post: { _id, loading, name, avatar, text, date, likes, comments, user }, // Post user
+  addLike,
+  removeLike,
 }) => {
   return (
     <div className='post bg-white p-1 my-1'>
@@ -21,11 +24,15 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button type='button' className='btn btn-light'>
+        <button type='button' className='btn btn-light' onClick={() => addLike(_id)}>
           <i className='fas fa-thumbs-up' />{' '}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button type='button' className='btn btn-light'>
+        <button
+          type='button'
+          className='btn btn-light'
+          onClick={() => removeLike(_id)}
+        >
           <i className='fas fa-thumbs-down' />
         </button>
         <Link to={`/post/${_id}`} className='btn btn-primary'>
@@ -47,10 +54,12 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   userID: PropTypes.string.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   userID: state.auth.user._id, // Logged in user Id
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
