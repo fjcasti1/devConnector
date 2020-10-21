bd0318f8b228d02e03b25185234f6d841011e00b
Francisco Castillo <fjcasti1@asu.edu>
2020-10-21 10:44:08 -0700

    Fix, links outside App with <a> not <Link>

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 const ProfileTop = ({
   profile: {
@@ -24,34 +23,34 @@ const ProfileTop = ({
 
         <div className='icons my-1'>
           {website && (
-            <Link to={website} target='_blank' rel='noopener noreferrer'>
+            <a href={website} target='_blank' rel='noopener noreferrer'>
               <i className='fas fa-globe fa-2x'></i>
-            </Link>
+            </a>
           )}
           {social && social.twitter && (
-            <Link to={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
               <i className='fab fa-twitter fa-2x'></i>
-            </Link>
+            </a>
           )}
           {social && social.facebook && (
-            <Link to={social.facebook} target='_blank' rel='noopener noreferrer'>
+            <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
               <i className='fab fa-facebook fa-2x'></i>
-            </Link>
+            </a>
           )}
           {social && social.linkedin && (
-            <Link to={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
               <i className='fab fa-linkedin fa-2x'></i>
-            </Link>
+            </a>
           )}
           {social && social.youtube && (
-            <Link to={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
               <i className='fab fa-youtube fa-2x'></i>
-            </Link>
+            </a>
           )}
           {social && social.instagram && (
-            <Link to={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
               <i className='fab fa-instagram fa-2x'></i>
-            </Link>
+            </a>
           )}
         </div>
       </div>
