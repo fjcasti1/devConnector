45029db6f55d8d6fbc6a35b43d62cd63fdf171e7
Francisco Castillo <fjcasti1@asu.edu>
2020-10-20 23:30:52 -0700

    Finish ProfileAbout component

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -8,25 +8,21 @@ const ProfileAbout = ({
     user: { name },
   },
 }) => (
-  <div class='profile-about bg-light p-2'>
+  <div className='profile-about bg-light p-2'>
     {bio && (
       <Fragment>
-        <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed doloremque
-          nesciunt, repellendus nostrum deleniti recusandae nobis neque modi
-          perspiciatis similique?
-        </p>
-        <div class='line'></div>
+        <h2 className='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
+        <p>{bio}</p>
+        <div className='line'></div>
       </Fragment>
     )}
     {skills.length > 0 && (
       <Fragment>
-        <h2 class='text-primary'>Skill Set</h2>
-        <div class='skills'>
+        <h2 className='text-primary'>Skill Set</h2>
+        <div className='skills'>
           {skills.map((skill, index) => (
-            <div key={index} class='p-1'>
-              <i class='fa fa-check'></i> {skill}
+            <div key={index} className='p-1'>
+              <i className='fa fa-check'></i> {skill}
             </div>
           ))}
         </div>
