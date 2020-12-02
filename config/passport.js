ecd149e9f7c5957d88d0b319811abdbe8df41c4c
Francisco Castillo <fjcasti1@asu.edu>
2020-12-02 13:23:37 -0700

    Add google user to database

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,26 @@ module.exports = function (passport) {
         callbackURL: '/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, cb) => {
+        const newUser = {
+          googleId: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: profile.photos[0].value,
+        };
+
+        try {
+          let user = await UserGoogle.findOne({ googleId: profile.id });
+          console.log(user);
+          if (!user) {
+            user = await UserGoogle.create(newUser);
+          }
+          console.log(user);
+          cb(null, user);
+        } catch (error) {
+          console.error(error);
+        }
+
         console.log(profile);
       },
     ),
