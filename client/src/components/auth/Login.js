593d3bf98256a1459e7ec415dcc2949b4a834a5f
Francisco Castillo <fjcasti1@asu.edu>
2020-12-03 01:14:31 -0700

    Add card with log in buttons

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -27,6 +27,23 @@ const Login = ({ login, isAuthenticated }) => {
 
   return (
     <Fragment>
+      <div className='login-container'>
+        <div className='card'>
+          <div className='card-title'>
+            <p className='lead'>
+              <i className='fas fa-user'></i> Sign Into Your Account
+            </p>
+          </div>
+          <div className='card-action'>
+            <a href='auth/google' className='btn btn-primary login-btn'>
+              Sign in with Google
+            </a>
+            <a href='auth/github' className='btn large btn-primary login-btn'>
+              Sign in with Github
+            </a>
+          </div>
+        </div>
+      </div>
       <h1 className='large text-primary'>Sign In</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Sign Into Your Account
@@ -51,9 +68,6 @@ const Login = ({ login, isAuthenticated }) => {
           />
         </div>
         <input type='submit' className='btn btn-primary' value='LogIn' />
-        <a href='auth/google' className='btn btn-primary'>
-          Sign in with Google
-        </a>
       </form>
       <p className='my-1'>
         Don't have an account? <Link to='/register'>Sign Up</Link>
