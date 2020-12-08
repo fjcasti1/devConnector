35a212d029fa9a7b497f01bd4878529d32728f3d
Francisco Castillo <fjcasti1@asu.edu>
2020-12-08 16:19:59 -0700

    Set Alert timeout to 2 secs

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 2000) => (dispatch) => {
   const id = uuidv4(); // Random ID
   dispatch({
     type: SET_ALERT,
