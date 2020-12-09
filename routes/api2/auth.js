268466c1a5a3d84796c38067bda1efb8a9e76059
Francisco Castillo <fjcasti1@asu.edu>
2020-12-08 22:38:09 -0700

    Try send dummy text from google callback

--- routes/api2/auth.js
+++ routes/api2/auth.js
@@ -4,7 +4,7 @@ import auth from '../../middleware/auth2.js';
 
 const router = express.Router();
 
-const REDIRECT = '/login';
+const REDIRECT = 'https://app-dev-general.herokuapp.com/dashboard';
 
 // @route     GET auth/user
 // @desc      Get authenticated user
@@ -33,7 +33,8 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect(REDIRECT);
+    // res.redirect(REDIRECT);
+    res.send('caca');
   },
 );
 
