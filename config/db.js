f0741def60c43a83dc97d79b6ebc2ef056e69f09
Francisco Castillo <fjcasti1@asu.edu>
2020-12-09 12:55:15 -0700

    Change MONGO_URI_DEV to MONGO_URI

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_DEV, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
