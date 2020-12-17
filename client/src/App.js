161ed9fbfbaeb6ef0199ea533ce0d7d5e906396e
Francisco Castillo <fjcasti1@asu.edu>
2020-12-17 12:51:53 -0700

    Restructured css files in file tree

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
