154e835b6ee11a000734a19d1f84471bf8983178
Francisco Castillo <fjcasti1@asu.edu>
2020-12-16 17:13:44 -0700

    Add 'api/' to routes & cors middleware & color log

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 import path from 'path';
 import morgan from 'morgan';
+import 'colors';
 import authRoutes from './routes/auth.js';
 import usersRoutes from './routes/users.js';
 import profileRoutes from './routes/profiles.js';
 import postsRoutes from './routes/posts.js';
 import passport from 'passport';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import connectMongo from 'connect-mongo';
 import session from 'express-session';
 // Passport config
 import './config/passportSetup.js';
+// Mongo config
+import connectDB from './config/db.js';
 
+// Connect session to MongoDB
 const MongoStore = connectMongo(session);
 
+// Access to env varaibles
 dotenv.config();
 
 // Initialize express server
@@ -43,15 +48,17 @@ app.use(
   }),
 );
 
+// Cors Middleware
+app.use(cors());
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Define Routes
-app.use('/auth', authRoutes);
-app.use('/users', usersRoutes);
-app.use('/profiles', profileRoutes);
-app.use('/posts', postsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/profiles', profileRoutes);
+app.use('/api/posts', postsRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -61,10 +68,17 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+      .underline,
+  ),
 );
