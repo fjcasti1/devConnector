fb3702a13579db3617ca1290446f47015ad1a393
Francisco Castillo <fjcasti1@asu.edu>
2020-12-17 17:38:33 -0700

    Add LoginButton Component. Buttons look better

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,23 +1,21 @@
 import React, { Fragment } from 'react';
+import LoginButton from './LoginButton';
 
 const Login = () => {
   return (
     <Fragment>
-      <div className='login-container'>
-        <div className='card'>
-          <div className='card-title'>
-            <p className='lead py-1'>
-              <i className='fas fa-user'></i> Sign Into Your Account
-            </p>
-          </div>
-          <div className='card-action'>
-            <a href='api/auth/google' className='btn btn-primary login-btn'>
-              <span className='hide-sm'>Continue with </span>Google
-            </a>
-            <a href='api/auth/github' className='btn btn-primary login-btn'>
-              <span className='hide-sm'>Continue with </span>GitHub
-            </a>
-          </div>
+      <div className='card login-container'>
+        <div className='card-title'>
+          <p className='lead py-1'>
+            <i className='fas fa-user'></i> Sign Into Your Account
+          </p>
+        </div>
+        <div className='card-action'>
+          <LoginButton provider='google' />
+          <LoginButton provider='github' name='GitHub' />
+          <LoginButton provider='facebook' />
+          <p>or</p>
+          <LoginButton provider='guest' />
         </div>
       </div>
     </Fragment>
---
+++ client/src/components/auth/LoginButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LoginButton = ({ provider, name = null }) => {
+  let continueText = '';
+  let iconClass = '';
+  if (!name) {
+    name = provider.charAt(0).toUpperCase() + provider.toLowerCase().slice(1);
+  }
+  if (provider === 'guest') {
+    iconClass = 'fas fa-user-circle';
+    continueText = 'Continue as ';
+  } else {
+    iconClass = `fab fa-${provider}`;
+    continueText = 'Continue with ';
+  }
+
+  return (
+    <a
+      href={`api/auth/${provider}`}
+      className={`btn btn-primary login-btn login-btn-${provider}`}
+    >
+      <i className={`${iconClass} fa-lg login-btn-icon`}></i>
+      <span className='login-btn-text'>
+        <span className='hide-md'>{continueText}</span> {name}
+      </span>
+    </a>
+  );
+};
+
+export default LoginButton;
